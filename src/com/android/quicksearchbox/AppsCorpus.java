3f00b91235290a95a8ff9394a3612935d602ef20
Bjorn Bringert <bringert@android.com>
2010-04-03 11:31:37 +0100

    Allow Apps corpus Go button to use a custom search activity
    Bug: http://b/issue?id=2559078
    Change-Id: Id8fbdecb42e8cb21ef57085fb9c35df0d347fcdb

--- src/com/android/quicksearchbox/AppsCorpus.java
+++ src/com/android/quicksearchbox/AppsCorpus.java
@@ -19,14 +19,22 @@ package com.android.quicksearchbox;
 
 import com.android.quicksearchbox.util.Util;
 
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 
 /**
  * The apps search source.
  */
 public class AppsCorpus extends SingleSourceCorpus {
+
+    private static final String TAG = "QSB.AppsCorpus";
 
     private static final String APPS_CORPUS_NAME = "apps";
 
@@ -65,4 +73,43 @@ public class AppsCorpus extends SingleSourceCorpus {
         return getContext().getText(R.string.corpus_description_apps);
     }
 
+    @Override
+    public Intent createSearchIntent(String query, Bundle appData) {
+        Intent appSearchIntent = createAppSearchIntent(query, appData);
+        if (appSearchIntent != null) {
+            return appSearchIntent;
+        } else {
+            // Fall back to sending the intent to ApplicationsProvider
+            return super.createSearchIntent(query, appData);
+        }
+    }
+
+    /**
+     * Creates an intent that starts the search activity specified in
+     * R.string.apps_search_activity.
+     *
+     * @return An intent, or {@code null} if the search activity is not set or can't be found.
+     */
+    private Intent createAppSearchIntent(String query, Bundle appData) {
+        ComponentName name = getComponentName(getContext(), R.string.apps_search_activity);
+        if (name == null) return null;
+        Intent intent = SearchableSource.createSourceSearchIntent(name, query, appData);
+        if (intent == null) return null;
+        ActivityInfo ai = intent.resolveActivityInfo(getContext().getPackageManager(), 0);
+        if (ai != null) {
+            return intent;
+        } else {
+            Log.w(TAG, "Can't find app search acitivity " + name);
+            return null;
+        }
+    }
+
+    private static ComponentName getComponentName(Context context, int res) {
+        String nameStr = context.getString(res);
+        if (TextUtils.isEmpty(nameStr)) {
+            return null;
+        } else {
+            return ComponentName.unflattenFromString(nameStr);
+        }
+    }
 }
