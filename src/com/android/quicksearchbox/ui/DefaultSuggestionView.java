CyanogenMod/android_packages_apps_QuickSearchBox

commit e29d52aa72c96c3147fa91d83aeb8dafc6d1f578

Load icons from other processes in a seperate thread.
This protects againts the process not responding, which previously caused QSB to
lock up. We also protect against the process throwing an exception in the same
case.

This change causes some icons to load after the suggestions are displayed. For
example, contact photos are displayed shortly after the contacts show, but the
quick contact badge is visible immediately. This only happens for suggestions
with content:// URIs for the icons.

Bug: 3064103

Change-Id: I23f36a82d381ea76897d65fe2f3ec46f6642b7d5

src/com/android/quicksearchbox/ui/DefaultSuggestionView.java
@@ -21,6 +21,8 @@ import com.android.quicksearchbox.R;
 import com.android.quicksearchbox.Source;
 import com.android.quicksearchbox.Suggestion;
 import com.android.quicksearchbox.SuggestionFormatter;
+import com.android.quicksearchbox.util.Consumer;
+import com.android.quicksearchbox.util.NowOrLater;
 
 import android.content.Context;
 import android.content.res.ColorStateList;
@@ -45,14 +47,17 @@ import android.widget.TextView;
 public class DefaultSuggestionView extends RelativeLayout implements SuggestionView {
 
     private static final boolean DBG = false;
-    private static final String TAG = "QSB.SuggestionView";
 
     public static final String VIEW_ID = "default";
 
+    private static int sId = 0;
+    // give the TAG an unique ID to make debugging easier (there are lots of these!)
+    private final String TAG = "QSB.SuggestionView:" + (sId++);
+
     private TextView mText1;
     private TextView mText2;
-    private ImageView mIcon1;
-    private ImageView mIcon2;
+    private AsyncIcon mIcon1;
+    private AsyncIcon mIcon2;
     private final SuggestionFormatter mSuggestionFormatter;
     private boolean mRefineable;
     private int mPosition;
@@ -79,8 +84,18 @@ public class DefaultSuggestionView extends RelativeLayout implements SuggestionV
         super.onFinishInflate();
         mText1 = (TextView) findViewById(R.id.text1);
         mText2 = (TextView) findViewById(R.id.text2);
-        mIcon1 = (ImageView) findViewById(R.id.icon1);
-        mIcon2 = (ImageView) findViewById(R.id.icon2);
+        mIcon1 = new AsyncIcon((ImageView) findViewById(R.id.icon1)){
+            // override default icon (when no other available) with default source icon
+            @Override
+            protected String getFallbackIconId(Source source) {
+                return source.getSourceIconUri().toString();
+            }
+            @Override
+            protected Drawable getFallbackIcon(Source source) {
+                return source.getSourceIcon();
+            }
+        };
+        mIcon2 = new AsyncIcon((ImageView) findViewById(R.id.icon2));
         // for some reason, creating mKeyListener inside the constructor causes it not to work.
         mKeyListener = new KeyListener();
 
@@ -98,11 +113,9 @@ public class DefaultSuggestionView extends RelativeLayout implements SuggestionV
         } else {
             text2 = formatText(suggestion.getSuggestionText2(), suggestion, null);
         }
-        Drawable icon1 = getSuggestionDrawableIcon1(suggestion);
-        Drawable icon2 = getSuggestionDrawableIcon2(suggestion);
         if (DBG) {
-            Log.d(TAG, "bindAsSuggestion(), text1=" + text1 + ",text2=" + text2
-                    + ",icon1=" + icon1 + ",icon2=" + icon2);
+            Log.d(TAG, "bindAsSuggestion(), text1=" + text1 + ",text2=" + text2 + ", q='" +
+                    userQuery + "'");
         }
         // If there is no text for the second line, allow the first line to be up to two lines
         if (TextUtils.isEmpty(text2)) {
@@ -116,8 +129,8 @@ public class DefaultSuggestionView extends RelativeLayout implements SuggestionV
         }
         setText1(text1);
         setText2(text2);
-        setIcon1(icon1);
-        setIcon2(icon2, null);
+        mIcon1.set(suggestion.getSuggestionSource(), suggestion.getSuggestionIcon1());
+        mIcon2.set(suggestion.getSuggestionSource(), suggestion.getSuggestionIcon2());
         updateRefinable(suggestion);
     }
 
@@ -129,28 +142,28 @@ public class DefaultSuggestionView extends RelativeLayout implements SuggestionV
     protected void updateRefinable(Suggestion suggestion) {
         mRefineable =
                 suggestion.isWebSearchSuggestion()
-                && mIcon2.getDrawable() == null
+                && mIcon2.getView().getDrawable() == null
                 && !TextUtils.isEmpty(suggestion.getSuggestionQuery());
         setRefinable(suggestion, mRefineable);
     }
 
     protected void setRefinable(Suggestion suggestion, boolean refinable) {
         if (refinable) {
-            mIcon2.setOnClickListener(new View.OnClickListener() {
+            mIcon2.getView().setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
                     onSuggestionQueryRefineClicked();
                 }
             });
-            mIcon2.setFocusable(true);
-            mIcon2.setOnKeyListener(mKeyListener);
+            mIcon2.getView().setFocusable(true);
+            mIcon2.getView().setOnKeyListener(mKeyListener);
             Drawable icon2 = getContext().getResources().getDrawable(R.drawable.edit_query);
             Drawable background =
                     getContext().getResources().getDrawable(R.drawable.edit_query_background);
-            setIcon2(icon2, background);
+            mIcon2.setDrawable(icon2, background, String.valueOf(R.drawable.edit_query));
         } else {
-            mIcon2.setOnClickListener(null);
-            mIcon2.setFocusable(false);
-            mIcon2.setOnKeyListener(null);
+            mIcon2.getView().setOnClickListener(null);
+            mIcon2.getView().setFocusable(false);
+            mIcon2.getView().setOnKeyListener(null);
         }
     }
 
@@ -161,19 +174,6 @@ public class DefaultSuggestionView extends RelativeLayout implements SuggestionV
                 0, url.length(),
                 Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
         return text;
-    }
-
-    public Drawable getSuggestionDrawableIcon1(Suggestion suggestion) {
-        Source source = suggestion.getSuggestionSource();
-        String iconId = suggestion.getSuggestionIcon1();
-        Drawable icon1 = iconId == null ? null : source.getIcon(iconId);
-        return icon1 == null ? source.getSourceIcon() : icon1;
-    }
-
-    public Drawable getSuggestionDrawableIcon2(Suggestion suggestion) {
-        Source source = suggestion.getSuggestionSource();
-        String iconId = suggestion.getSuggestionIcon2();
-        return iconId == null ? null : source.getIcon(iconId);
     }
 
     private CharSequence formatText(String str, Suggestion suggestion,
@@ -214,21 +214,6 @@ public class DefaultSuggestionView extends RelativeLayout implements SuggestionV
         } else {
             mText2.setVisibility(VISIBLE);
         }
-    }
-
-    /**
-     * Sets the left-hand-side icon.
-     */
-    private void setIcon1(Drawable icon) {
-        setViewDrawable(mIcon1, icon);
-    }
-
-    /**
-     * Sets the right-hand-side icon and its background.
-     */
-    private void setIcon2(Drawable icon, Drawable background) {
-        setViewDrawable(mIcon2, icon);
-        mIcon2.setBackgroundDrawable(background);
     }
 
     /**
@@ -296,10 +281,10 @@ public class DefaultSuggestionView extends RelativeLayout implements SuggestionV
         public boolean onKey(View v, int keyCode, KeyEvent event) {
             boolean consumed = false;
             if (event.getAction() == KeyEvent.ACTION_DOWN) {
-                if (keyCode == KeyEvent.KEYCODE_DPAD_RIGHT && v != mIcon2) {
-                    consumed = mIcon2.requestFocus();
+                if (keyCode == KeyEvent.KEYCODE_DPAD_RIGHT && v != mIcon2.getView()) {
+                    consumed = mIcon2.getView().requestFocus();
                     if (DBG) Log.d(TAG, "onKey Icon2 accepted focus: " + consumed);
-                } else if (keyCode == KeyEvent.KEYCODE_DPAD_LEFT && v == mIcon2) {
+                } else if (keyCode == KeyEvent.KEYCODE_DPAD_LEFT && v == mIcon2.getView()) {
                     consumed = requestFocus();
                     if (DBG) Log.d(TAG, "onKey SuggestionView accepted focus: " + consumed);
                 }
@@ -308,4 +293,87 @@ public class DefaultSuggestionView extends RelativeLayout implements SuggestionV
         }
     }
 
+    private class AsyncIcon {
+        private final ImageView mView;
+        private String mCurrentId;
+        private String mWantedId;
+
+        public AsyncIcon(ImageView view) {
+            mView = view;
+        }
+
+        public void set(final Source source, final String iconId) {
+            if (iconId != null) {
+                mWantedId = iconId;
+                if (!TextUtils.equals(mWantedId, mCurrentId)) {
+                    if (DBG) Log.d(TAG, "getting icon Id=" + iconId);
+                    NowOrLater<Drawable> icon = source.getIcon(iconId);
+                    if (icon.haveNow()) {
+                        if (DBG) Log.d(TAG, "getIcon ready now");
+                        handleNewDrawable(icon.getNow(), iconId, source);
+                    } else {
+                        // make sure old icon is not visible while new one is loaded
+                        if (DBG) Log.d(TAG , "getIcon getting later");
+                        clearDrawable();
+                        icon.getLater(new Consumer<Drawable>(){
+                            public boolean consume(Drawable icon) {
+                                if (DBG) {
+                                    Log.d(TAG, "IconConsumer.consume got id " + iconId +
+                                            " want id " + mWantedId);
+                                }
+                                // ensure we have not been re-bound since the request was made.
+                                if (TextUtils.equals(iconId, mWantedId)) {
+                                    handleNewDrawable(icon, iconId, source);
+                                    return true;
+                                }
+                                return false;
+                            }});
+                    }
+                }
+            } else {
+                mWantedId = null;
+                handleNewDrawable(null, null, source);
+            }
+        }
+
+        public ImageView getView() {
+            return mView;
+        }
+
+        private void handleNewDrawable(Drawable icon, String id, Source source) {
+            if (icon == null) {
+                mWantedId = getFallbackIconId(source);
+                if (TextUtils.equals(mWantedId, mCurrentId)) {
+                    return;
+                }
+                icon = getFallbackIcon(source);
+            }
+            setDrawable(icon, id);
+        }
+
+        public void setDrawable(Drawable icon, Drawable background, String id) {
+            mCurrentId = id;
+            mView.setImageDrawable(icon);
+            mView.setBackgroundDrawable(background);
+        }
+
+        private void setDrawable(Drawable icon, String id) {
+            mCurrentId = id;
+            setViewDrawable(mView, icon);
+        }
+
+        private void clearDrawable() {
+            mCurrentId = null;
+            mView.setImageDrawable(null);
+        }
+
+        protected String getFallbackIconId(Source source) {
+            return null;
+        }
+
+        protected Drawable getFallbackIcon(Source source) {
+            return null;
+        }
+
+    }
 }

src/com/android/quicksearchbox/AbstractInternalSource.java
@@ -18,14 +18,15 @@ package com.android.quicksearchbox;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Handler;
 
 /**
  * Abstract implementation of a source that is not backed by a searchable activity.
  */
 public abstract class AbstractInternalSource extends AbstractSource {
 
-    public AbstractInternalSource(Context context) {
-        super(context);
+    public AbstractInternalSource(Context context, Handler uiThread) {
+        super(context, uiThread);
     }
 
     public String getSuggestUri() {

src/com/android/quicksearchbox/AbstractSource.java
@@ -17,6 +17,7 @@
 package com.android.quicksearchbox;
 
 import com.android.quicksearchbox.ui.SuggestionViewFactory;
+import com.android.quicksearchbox.util.NowOrLater;
 
 import android.app.SearchManager;
 import android.content.ComponentName;
@@ -25,6 +26,7 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 
 /**
@@ -35,13 +37,15 @@ public abstract class AbstractSource implements Source {
     private static final String TAG = "QSB.AbstractSource";
 
     private final Context mContext;
+    private final Handler mUiThread;
 
     private IconLoader mIconLoader;
 
     private SuggestionViewFactory mViewFactory;
 
-    public AbstractSource(Context context) {
+    public AbstractSource(Context context, Handler uiThread) {
         mContext = context;
+        mUiThread = uiThread;
     }
 
     protected Context getContext() {
@@ -51,7 +55,8 @@ public abstract class AbstractSource implements Source {
     protected IconLoader getIconLoader() {
         if (mIconLoader == null) {
             String iconPackage = getIconPackage();
-            mIconLoader = new CachingIconLoader(new PackageIconLoader(mContext, iconPackage));
+            mIconLoader = new CachingIconLoader(
+                    new PackageIconLoader(mContext, iconPackage, mUiThread));
         }
         return mIconLoader;
     }
@@ -62,7 +67,7 @@ public abstract class AbstractSource implements Source {
         return getVersionCode() == version;
     }
 
-    public Drawable getIcon(String drawableId) {
+    public NowOrLater<Drawable> getIcon(String drawableId) {
         return getIconLoader().getIcon(drawableId);
     }
 

src/com/android/quicksearchbox/CachingIconLoader.java
@@ -16,6 +16,12 @@
 
 package com.android.quicksearchbox;
 
+import com.android.quicksearchbox.util.CachedLater;
+import com.android.quicksearchbox.util.Consumer;
+import com.android.quicksearchbox.util.Now;
+import com.android.quicksearchbox.util.NowOrLater;
+import com.android.quicksearchbox.util.NowOrLaterWrapper;
+
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.text.TextUtils;
@@ -34,7 +40,7 @@ public class CachingIconLoader implements IconLoader {
 
     private final IconLoader mWrapped;
 
-    private final WeakHashMap<String, Drawable.ConstantState> mIconCache;
+    private final WeakHashMap<String, Entry> mIconCache;
 
     /**
      * Creates a new caching icon loader.
@@ -43,20 +49,33 @@ public class CachingIconLoader implements IconLoader {
      */
     public CachingIconLoader(IconLoader wrapped) {
         mWrapped = wrapped;
-        mIconCache = new WeakHashMap<String, Drawable.ConstantState>();
+        mIconCache = new WeakHashMap<String, Entry>();
     }
 
-    public Drawable getIcon(String drawableId) {
+    public NowOrLater<Drawable> getIcon(String drawableId) {
         if (DBG) Log.d(TAG, "getIcon(" + drawableId + ")");
         if (TextUtils.isEmpty(drawableId) || "0".equals(drawableId)) {
-            return null;
+            return new Now<Drawable>(null);
         }
-        Drawable drawable = checkIconCache(drawableId);
-        if (drawable != null) {
-            return drawable;
+        Entry newEntry = null;
+        NowOrLater<Drawable.ConstantState> drawableState;
+        synchronized (this) {
+            drawableState = queryCache(drawableId);
+            if (drawableState == null) {
+                newEntry = new Entry();
+                storeInIconCache(drawableId, newEntry);
+            }
         }
-        drawable = mWrapped.getIcon(drawableId);
-        storeInIconCache(drawableId, drawable);
+        if (drawableState != null) {
+            return new NowOrLaterWrapper<Drawable.ConstantState, Drawable>(drawableState){
+                @Override
+                public Drawable get(Drawable.ConstantState value) {
+                    return value == null ? null : value.newDrawable();
+                }};
+        }
+        NowOrLater<Drawable> drawable = mWrapped.getIcon(drawableId);
+        newEntry.set(drawable);
+        storeInIconCache(drawableId, newEntry);
         return drawable;
     }
 
@@ -64,18 +83,58 @@ public class CachingIconLoader implements IconLoader {
         return mWrapped.getIconUri(drawableId);
     }
 
-    private Drawable checkIconCache(String drawableId) {
-        Drawable.ConstantState cached = mIconCache.get(drawableId);
-        if (cached == null) {
-            return null;
+    private synchronized NowOrLater<Drawable.ConstantState> queryCache(String drawableId) {
+        NowOrLater<Drawable.ConstantState> cached = mIconCache.get(drawableId);
+        if (DBG) {
+            if (cached != null) Log.d(TAG, "Found icon in cache: " + drawableId);
         }
-        if (DBG) Log.d(TAG, "Found icon in cache: " + drawableId);
-        return cached.newDrawable();
+        return cached;
     }
 
-    private void storeInIconCache(String resourceUri, Drawable drawable) {
+    private synchronized void storeInIconCache(String resourceUri, Entry drawable) {
         if (drawable != null) {
-            mIconCache.put(resourceUri, drawable.getConstantState());
+            mIconCache.put(resourceUri, drawable);
         }
     }
+
+    private static class Entry extends CachedLater<Drawable.ConstantState>
+            implements Consumer<Drawable>{
+        private NowOrLater<Drawable> mDrawable;
+        private boolean mGotDrawable;
+        private boolean mCreateRequested;
+
+        public Entry() {
+        }
+
+        public synchronized void set(NowOrLater<Drawable> drawable) {
+            if (mGotDrawable) throw new IllegalStateException("set() may only be called once.");
+            mGotDrawable = true;
+            mDrawable = drawable;
+            if (mCreateRequested) {
+                getLater();
+            }
+        }
+
+        @Override
+        protected synchronized void create() {
+            if (!mCreateRequested) {
+                mCreateRequested = true;
+                if (mGotDrawable) {
+                    getLater();
+                }
+            }
+        }
+
+        private void getLater() {
+            NowOrLater<Drawable> drawable = mDrawable;
+            mDrawable = null;
+            drawable.getLater(this);
+        }
+
+        public boolean consume(Drawable value) {
+            store(value == null ? null : value.getConstantState());
+            return true;
+        }
+    }
+
 }

src/com/android/quicksearchbox/ContactsSource.java
@@ -22,14 +22,16 @@ import com.android.quicksearchbox.ui.SuggestionViewInflater;
 import android.app.SearchableInfo;
 import android.content.Context;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Handler;
 
 /**
  * Special case SearchableSource for contacts to provide the custom contacts suggestion view.
  */
 public class ContactsSource extends SearchableSource {
 
-    public ContactsSource(Context context, SearchableInfo searchable) throws NameNotFoundException {
-        super(context, searchable);
+    public ContactsSource(Context context, SearchableInfo searchable, Handler uiThread)
+            throws NameNotFoundException {
+        super(context, searchable, uiThread);
     }
 
     @Override

src/com/android/quicksearchbox/DefaultCorpusRanker.java
@@ -16,7 +16,7 @@
 
 package com.android.quicksearchbox;
 
-import com.android.quicksearchbox.util.AsyncCache;
+import com.android.quicksearchbox.util.CachedLater;
 import com.android.quicksearchbox.util.Consumer;
 
 import android.database.DataSetObserver;
@@ -59,7 +59,7 @@ public class DefaultCorpusRanker implements CorpusRanker {
     }
 
     public void getCorporaInAll(Consumer<List<Corpus>> consumer) {
-        mRankedCorpora.get(consumer);
+        mRankedCorpora.getLater(consumer);
     }
 
     public void clear() {
@@ -73,8 +73,9 @@ public class DefaultCorpusRanker implements CorpusRanker {
         }
     }
 
-    private class RankedCorporaCache extends AsyncCache<List<Corpus>> {
+    private class RankedCorporaCache extends CachedLater<List<Corpus>> {
 
+        @Override
         protected void create() {
             mShortcuts.getCorpusScores(new Consumer<Map<String,Integer>>(){
                 public boolean consume(Map<String, Integer> clickScores) {

src/com/android/quicksearchbox/IconLoader.java
@@ -16,6 +16,8 @@
 
 package com.android.quicksearchbox;
 
+import com.android.quicksearchbox.util.NowOrLater;
+
 import android.content.ContentResolver;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
@@ -37,15 +39,14 @@ public interface IconLoader {
      *
      * All resources and URIs are read using the suggestion provider's context.
      *
-     * If the ID is not formatted as expected, or no drawable can be found for
-     * the provided value, this method returns null.
+     * @return a {@link NowOrLater} for retrieving the icon. If the ID is not formatted as expected,
+     *      or no drawable can be found for the provided value, the value from this will be null.
      *
      * @param drawableId a string like "2130837524",
      *        "android.resource://com.android.alarmclock/2130837524",
      *        or "content://contacts/photos/253".
-     * @return a Drawable, or {@code null} if none found
      */
-    Drawable getIcon(String drawableId);
+    NowOrLater<Drawable> getIcon(String drawableId);
 
     /**
      * Converts a drawable ID to a Uri that can be used from other packages.

src/com/android/quicksearchbox/PackageIconLoader.java
@@ -16,6 +16,9 @@
 
 package com.android.quicksearchbox;
 
+import com.android.quicksearchbox.util.CachedLater;
+import com.android.quicksearchbox.util.Now;
+import com.android.quicksearchbox.util.NowOrLater;
 import com.android.quicksearchbox.util.Util;
 
 import android.content.ContentResolver;
@@ -25,6 +28,9 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Process;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -43,11 +49,18 @@ public class PackageIconLoader implements IconLoader {
     private static final boolean DBG = false;
     private static final String TAG = "QSB.PackageIconLoader";
 
+    private static final String RESOURCE_URI_SCHEME = "android.resource";
+
     private final Context mContext;
 
     private final String mPackageName;
 
     private Context mPackageContext;
+
+    private final Handler mUiThread;
+
+    private final HandlerThread mIconLoaderThread;
+    private final Handler mIconLoaderHandler;
 
     /**
      * Creates a new icon loader.
@@ -57,9 +70,14 @@ public class PackageIconLoader implements IconLoader {
      *        Resource IDs without an explicit package will be resolved against the package
      *        of this context.
      */
-    public PackageIconLoader(Context context, String packageName) {
+    public PackageIconLoader(Context context, String packageName, Handler uiThread) {
         mContext = context;
         mPackageName = packageName;
+        mUiThread = uiThread;
+        mIconLoaderThread = new HandlerThread("Icon loader " + packageName,
+                Process.THREAD_PRIORITY_BACKGROUND);
+        mIconLoaderThread.start();
+        mIconLoaderHandler = new Handler(mIconLoaderThread.getLooper());
     }
 
     private boolean ensurePackageContext() {
@@ -76,26 +94,36 @@ public class PackageIconLoader implements IconLoader {
         return true;
     }
 
-    public Drawable getIcon(String drawableId) {
+    public NowOrLater<Drawable> getIcon(final String drawableId) {
         if (DBG) Log.d(TAG, "getIcon(" + drawableId + ")");
         if (TextUtils.isEmpty(drawableId) || "0".equals(drawableId)) {
-            return null;
+            return new Now<Drawable>(null);
         }
-        if (!ensurePackageContext()) return null;
+        if (!ensurePackageContext()) {
+            return new Now<Drawable>(null);
+        }
+        NowOrLater<Drawable> drawable;
         try {
             // First, see if it's just an integer
             int resourceId = Integer.parseInt(drawableId);
             // If so, find it by resource ID
-            return mPackageContext.getResources().getDrawable(resourceId);
+            Drawable icon = mPackageContext.getResources().getDrawable(resourceId);
+            drawable = new Now<Drawable>(icon);
         } catch (NumberFormatException nfe) {
             // It's not an integer, use it as a URI
             Uri uri = Uri.parse(drawableId);
-            return getDrawable(uri);
+            if (RESOURCE_URI_SCHEME.equals(uri.getScheme())) {
+                // load all resources synchronously, to reduce UI flickering
+                drawable = new Now<Drawable>(getDrawable(uri));
+            } else {
+                drawable = new IconLaterTask(uri);
+            }
         } catch (Resources.NotFoundException nfe) {
             // It was an integer, but it couldn't be found, bail out
             Log.w(TAG, "Icon resource not found: " + drawableId);
-            return null;
+            drawable = new Now<Drawable>(null);
         }
+        return drawable;
     }
 
     public Uri getIconUri(String drawableId) {
@@ -196,5 +224,38 @@ public class PackageIconLoader implements IconLoader {
         res.r = r;
         res.id = id;
         return res;
+    }
+
+    private class IconLaterTask extends CachedLater<Drawable> implements Runnable {
+        private final Uri mUri;
+
+        public IconLaterTask(Uri iconUri) {
+            mUri = iconUri;
+        }
+
+        @Override
+        protected void create() {
+            mIconLoaderHandler.post(this);
+        }
+
+        @Override
+        public void run() {
+            final Drawable icon = getIcon();
+            mUiThread.post(new Runnable(){
+                public void run() {
+                    store(icon);
+                }});
+        }
+
+        private Drawable getIcon() {
+            try {
+                return getDrawable(mUri);
+            } catch (Throwable t) {
+                // we're making a call into another package, which could throw any exception.
+                // Make sure it doesn't crash QSB
+                Log.e(TAG, "Failed to load icon " + mUri, t);
+                return null;
+            }
+        }
     }
 }

commit 5c383769c9cf23ebb38e03f1245007b8f7e48950

Fix query refinement.
Regression was introduced in change e29d52aa72c96c3147fa91d83aeb8dafc6d1f578.

Change-Id: I878556faf07fa10eaf172d22ef2f859d26407c81

src/com/android/quicksearchbox/ui/DefaultSuggestionView.java
@@ -144,6 +144,7 @@ public class DefaultSuggestionView extends RelativeLayout implements SuggestionV
                 suggestion.isWebSearchSuggestion()
                 && mIcon2.getView().getDrawable() == null
                 && !TextUtils.isEmpty(suggestion.getSuggestionQuery());
+        if (DBG) Log.d(TAG, "updateRefinable: " + mRefineable);
         setRefinable(suggestion, mRefineable);
     }
 
@@ -353,7 +354,8 @@ public class DefaultSuggestionView extends RelativeLayout implements SuggestionV
 
         public void setDrawable(Drawable icon, Drawable background, String id) {
             mCurrentId = id;
-            mView.setImageDrawable(icon);
+            mWantedId = id;
+            setViewDrawable(mView, icon);
             mView.setBackgroundDrawable(background);
         }
 