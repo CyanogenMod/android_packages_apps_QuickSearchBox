c6e4a0851905e2702c56adea162b70c7204adf72
Mathew Inwood <mathewi@google.com>
2010-11-03 13:55:34 +0000

    Refactor logic out of settings screen.
    This is to allow a two-pane settings screen to exist in parallel with
    the eisting (deprecated) version. We need to keep the existing one to
    allow continued support for Gingerbread & Froyo.
    
    Dependant on change: I03a93f6d6394e09c4accff5c7fc1817a98ad828a
    
    Change-Id: Id25009e2ad68cbb3fa8cf77f84b2371b2e833e23

--- src/com/android/quicksearchbox/SearchSettingsActivity.java
+++ src/com/android/quicksearchbox/SearchSettingsActivity.java
@@ -16,106 +16,45 @@
 
 package com.android.quicksearchbox;
 
-import com.android.quicksearchbox.util.Consumer;
-import com.android.quicksearchbox.util.Consumers;
-
 import android.os.Bundle;
-import android.os.Handler;
-import android.preference.Preference;
-import android.preference.Preference.OnPreferenceChangeListener;
-import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
-import android.util.Log;
 
 /**
  * Activity for setting global search preferences.
  */
-public class SearchSettingsActivity extends PreferenceActivity
-        implements OnPreferenceClickListener, OnPreferenceChangeListener {
+public class SearchSettingsActivity extends PreferenceActivity {
 
-    private static final boolean DBG = false;
-    private static final String TAG = "QSB.SearchSettingsActivity";
-
-    // Name of the preferences file used to store search preference
-    public static final String PREFERENCES_NAME = "SearchSettings";
-
-    // Intent action that opens the "Searchable Items" preference
-    public static final String ACTION_SEARCHABLE_ITEMS =
-            "com.android.quicksearchbox.action.SEARCHABLE_ITEMS";
-
-    // Only used to find the preferences after inflating
-    private static final String CLEAR_SHORTCUTS_PREF = "clear_shortcuts";
-    private static final String SEARCH_CORPORA_PREF = "search_corpora";
-
-    // References to the top-level preference objects
-    private OkCancelPreference mClearShortcutsPreference;
-
-    private Handler mHandler = new Handler();
+    private SearchSettingsController mController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getPreferenceManager().setSharedPreferencesName(PREFERENCES_NAME);
+        mController = createController();
+
+        getPreferenceManager().setSharedPreferencesName(mController.getPreferencesName());
 
         addPreferencesFromResource(R.xml.preferences);
 
-        mClearShortcutsPreference =
-                (OkCancelPreference) findPreference(CLEAR_SHORTCUTS_PREF);
-        mClearShortcutsPreference.setListener(new OkCancelPreference.Listener() {
-            @Override
-            public void onDialogClosed(boolean okClicked) {
-                if (okClicked) {
-                    clearShortcuts();
-                }
-            }
-        });
+        mController.setClearShortcutsPreference((OkCancelPreference)
+                findPreference(mController.getClearShortcutsPreferenceName()));
 
-        Preference corporaPreference = findPreference(SEARCH_CORPORA_PREF);
-        corporaPreference.setIntent(getSettings().getSearchableItemsIntent());
+        mController.setCorporaPreference(findPreference(mController.getCorporaPreferenceName()));
+    }
+
+    protected SearchSettingsController createController() {
+        QsbApplication app = QsbApplication.get(this);
+        return new SearchSettingsController(app.getSettings(), app.getShortcutRepository());
+    }
+
+    protected SearchSettingsController getController() {
+        return mController;
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        updateClearShortcutsPreference();
-    }
-
-    private SearchSettings getSettings() {
-        return QsbApplication.get(this).getSettings();
-    }
-
-    private ShortcutRepository getShortcuts() {
-        return QsbApplication.get(this).getShortcutRepository();
-    }
-
-    /**
-     * Enables/disables the "Clear search shortcuts" preference depending
-     * on whether there is any search history.
-     */
-    private void updateClearShortcutsPreference() {
-        getShortcuts().hasHistory(Consumers.createAsyncConsumer(mHandler, new Consumer<Boolean>() {
-            @Override
-            public boolean consume(Boolean hasHistory) {
-                if (DBG) Log.d(TAG, "hasHistory()=" + hasHistory);
-                mClearShortcutsPreference.setEnabled(hasHistory);
-                return true;
-            }
-        }));
-    }
-
-    public boolean onPreferenceClick(Preference preference) {
-        return false;
-    }
-
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        return true;
-    }
-
-    private void clearShortcuts() {
-        Log.i(TAG, "Clearing shortcuts...");
-        getShortcuts().clearHistory();
-        mClearShortcutsPreference.setEnabled(false);
+        mController.onResume();
     }
 
 }
